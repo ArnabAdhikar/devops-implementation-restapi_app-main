4d2a44110a68cff869a70890fe31f981a9ffb96b
ArnabAdhikar <arnaba075@gmail.com>
2026-04-24 00:59:36 +0530

    v0.4.2

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -5,14 +5,12 @@ variable "sg_enable_ssh_https" {}
 variable "subnet_ids" {}
 variable "tag_name" {}
 variable "lb_target_group_arn" {}
-variable "ec2_instance_id" {}
 variable "lb_listner_port" {}
 variable "lb_listner_protocol" {}
 variable "lb_listner_default_action" {}
 variable "lb_https_listner_port" {}
 variable "lb_https_listner_protocol" {}
 variable "dev_proj_1_acm_arn" {}
-variable "lb_target_group_attachment_port" {}
 
 output "aws_lb_dns_name" {
   value = aws_lb.dev_proj_1_lb.dns_name
@@ -35,12 +33,6 @@ resource "aws_lb" "dev_proj_1_lb" {
   tags = {
     Name = "example-lb"
   }
-}
-
-resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment" {
-  target_group_arn = var.lb_target_group_arn
-  target_id        = var.ec2_instance_id # Replace with your EC2 instance reference
-  port             = var.lb_target_group_attachment_port
 }
 
 resource "aws_lb_listener" "dev_proj_1_lb_listner" {
